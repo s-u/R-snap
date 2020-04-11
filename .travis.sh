c5b7c1150b5d16de02ef212528a740fe38ac0183
Simon Urbanek <simon.urbanek@R-project.org>
2020-04-11 14:49:31 +1200

    travis build updates...

--- .travis.sh
+++ .travis.sh
@@ -20,8 +20,9 @@ if [ "$ACTION" = sysdeps ]; then
     sudo apt-get update -qq
     sudo apt-get install -y gcc g++ gfortran libcairo-dev libreadline-dev libxt-dev libjpeg-dev libicu-dev libssl-dev libcurl4-openssl-dev subversion git automake make libtool libtiff-dev libpcre2-dev liblzma-dev libbz2-dev gettext rsync curl openssh-client texinfo texlive
     ## install some tex pkgs by hand
-    sudo tlmgr update --self
-    sudo tlmgr install titling framed inconsolata
+    sudo -i tlmgr init-usertree
+    sudo -i tlmgr update --self
+    sudo -i tlmgr install titling framed inconsolata
 fi
 
 if [ "$ACTION" = build ]; then
