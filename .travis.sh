963f749e190b8660fbf0c484fc34291b05c209d3
Simon Urbanek <simon.urbanek@R-project.org>
2020-04-11 14:52:20 +1200

    travis build updates...

--- .travis.sh
+++ .travis.sh
@@ -18,7 +18,7 @@ if [ "$ACTION" = sysdeps ]; then
     ## for Ubuntu/Debian
     ## NOTE: texlive-fonts-extra is HUGE so we avoid it if we can ...
     sudo apt-get update -qq
-    sudo apt-get install -y gcc g++ gfortran libcairo-dev libreadline-dev libxt-dev libjpeg-dev libicu-dev libssl-dev libcurl4-openssl-dev subversion git automake make libtool libtiff-dev libpcre2-dev liblzma-dev libbz2-dev gettext rsync curl openssh-client texinfo texlive
+    sudo apt-get install -y gcc g++ gfortran libcairo-dev libreadline-dev libxt-dev libjpeg-dev libicu-dev libssl-dev libcurl4-openssl-dev subversion git automake make libtool libtiff-dev libpcre2-dev liblzma-dev libbz2-dev gettext rsync curl openssh-client texinfo texlive xzdec
     ## install some tex pkgs by hand
     sudo -i tlmgr init-usertree
     sudo -i tlmgr update --self
